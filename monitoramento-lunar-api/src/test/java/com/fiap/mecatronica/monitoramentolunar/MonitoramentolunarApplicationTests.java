6b1c1fdfc11824cb6aa39b479c92ccadb032d3b7
leonardokowata <leokowata@gmail.com>
2026-06-04 22:29:14 -0300

    feat: estrutura inicial do projeto lunar gs1

---
+++ monitoramento-lunar-api/src/test/java/com/fiap/mecatronica/monitoramentolunar/MonitoramentolunarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.mecatronica.monitoramentolunar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentolunarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
